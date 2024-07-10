fbfe98c7ed8d621b17d5c62a3c1619bf4456d145
Nelvb <169047839+Nelvb@users.noreply.github.com>
2024-07-10 09:31:58 +0000

    realizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let ext = [".com", ".es", ".net"];
+for (let p of pronoun) {
+  for (let a of adj) {
+    for (let n of noun)
+      for (let e of ext) {
+        console.log(p + a + n + e);
+      }
+  }
+}
